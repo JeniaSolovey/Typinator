bbf18ec98f15ef4eb7edba17caf40cfc1c0a5366
JeniaSolovey <jeniasolovey48@gmail.com>
2016-10-30 18:26:49 +0100

    Set random word for enemies

--- spawner.cpp
+++ spawner.cpp
@@ -1,13 +1,34 @@
 #include "spawner.h"
 #include "enemy.h"
+QString Spawner::getRandomWord()
+{
+    return words.at(qrand()%words.size()-1);
+}
+
 Spawner::Spawner(QGraphicsScene *scene)
 {
     this->scene = scene;
+    LoadWords();
+}
+
+void Spawner::LoadWords()
+{
+    QFile file(":/res/words.txt");
+    QByteArray arr;
+
+    if(file.open(QIODevice::ReadOnly |QIODevice::Text))
+        {
+            while(!file.atEnd())
+            {
+                QString str = file.readLine();
+                words.push_back(str);
+            }
+        }
 }
 
 void Spawner::SpawnEnemy()
 {
-        Enemy *enemy = new Enemy();
+        Enemy *enemy = new Enemy(getRandomWord());
         scene->addItem(enemy);
         enemy->setRandomPosition();
 }
